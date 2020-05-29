8365cec8b525f2a85a4d634fb6c15cdf5eefb9c9
andzapata <andzapata514@gmail.com>
2020-05-28 23:41:27 -0500

    [Fix] Builds

--- 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
+++ 0x04-unity_publishing/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,11 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool isFlat = true;
     public float speed = 1000f;
     public Rigidbody rb;
     private int score = 0;
@@ -13,6 +15,8 @@ public class PlayerController : MonoBehaviour
     public Text healthText;
     public Text winLoseText;
     public Image winLoseBG;
+
+    public GameObject joystick;
 
     public void Update()
     {
@@ -26,20 +30,42 @@ public class PlayerController : MonoBehaviour
         }
 
         if (Input.GetKey(KeyCode.Escape))
-        {
             SceneManager.LoadScene(0);
-        }
     }
+
     public void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
-            rb.AddForce(0, 0, speed * Time.deltaTime);
-        if (Input.GetKey(KeyCode.A))
-            rb.AddForce((-1 * speed) * Time.deltaTime, 0, 0);
-        if (Input.GetKey(KeyCode.S))
-            rb.AddForce(0, 0, (-1 * speed) * Time.deltaTime);
-        if (Input.GetKey(KeyCode.D))
-            rb.AddForce(speed * Time.deltaTime, 0, 0);
+        if (SystemInfo.deviceType == DeviceType.Handheld)
+        {
+            if (Input.touchCount > 0)
+            {
+                isFlat = false;
+                joystick.gameObject.SetActive(true);
+                float movH = CrossPlatformInputManager.GetAxis("Horizontal") * (speed * Time.deltaTime);
+                float movY = CrossPlatformInputManager.GetAxis("Vertical") * (speed * Time.deltaTime);
+                rb.AddForce(movH, 0, movY);
+            }
+            else
+            {
+                Vector3 tilt = Input.acceleration;
+                if (isFlat)
+                {
+                    tilt = Quaternion.Euler(90, 0, 0) * tilt;
+                    rb.AddForce(tilt);
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+                rb.AddForce(0, 0, speed * Time.deltaTime);
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+                rb.AddForce((-1 * speed) * Time.deltaTime, 0, 0);
+            if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+                rb.AddForce(0, 0, (-1 * speed) * Time.deltaTime);
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+                rb.AddForce(speed * Time.deltaTime, 0, 0);
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -55,7 +81,7 @@ public class PlayerController : MonoBehaviour
             health = health - 1;
             SetHealthText();
         }
-        
+
         if (other.tag == "Goal")
         {
             winLoseBG.color = new Color32(4, 86, 79, 200);
