91c702e31ed0dc0898155c551628692049bef964
andzapata <andzapata514@gmail.com>
2020-06-08 13:22:24 -0500

    [Fix] Jumping and camera init position

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -2,18 +2,33 @@
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 6f;
     public CharacterController controller;
+    public float jump = 2.5f;
+    public float speed = 12f;
+    public float gravity = -10f * 2;
+    Transform playerPosition;
+    Vector3 vel;
 
-    public void FixedUpdate()
+
+    void Start()
+    {
+        playerPosition = GetComponent<Transform>();
+    }
+    public void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
-
-        if (direction.magnitude >= 0.1f)
+        if (controller.isGrounded && vel.y < 0)
+            vel.y = -2f;
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+            vel.y = Mathf.Sqrt(jump * -2f * gravity);
+        Vector3 direction = (transform.right * horizontal) + (transform.forward * vertical);
+        controller.Move(direction * speed * Time.deltaTime);
+        vel.y += gravity * Time.deltaTime;
+        controller.Move(vel * Time.deltaTime);
+        if (playerPosition.position.y < -30f)
         {
-            controller.Move(direction * speed * Time.deltaTime);
+            playerPosition.position = new Vector3(0, 50, 0);
         }
     }
 }
