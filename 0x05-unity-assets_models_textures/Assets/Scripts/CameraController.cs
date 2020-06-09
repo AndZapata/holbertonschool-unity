c45b1e8e287e4f13f5860d15f4b4f03beab97b13
andzapata <andzapata514@gmail.com>
2020-06-08 19:18:42 -0500

    [Fix] TimerCanvas UI design

--- 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -2,31 +2,27 @@
 
 public class CameraController : MonoBehaviour
 {
-    public bool lookAtPlayer = true;
-    private Vector3 _cameraOffset;
+    private Vector3 cameraOffset;
     public Transform playerTransform;
     public bool rotateAroundPlayer = true;
     public float rotationSpeed = 5f;
-
     public float Smoothfactor = 1f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        _cameraOffset = transform.position - playerTransform.position;
+        cameraOffset = transform.position - playerTransform.position;
     }
 
-    // Update is called once per frame
     void LateUpdate()
     {
         if (rotateAroundPlayer)
         {
             Quaternion camTurnAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
-            _cameraOffset = camTurnAngle * _cameraOffset;
+            cameraOffset = camTurnAngle * cameraOffset;
         }
-        Vector3 newPos = playerTransform.position + _cameraOffset;
-        transform.position = Vector3.Slerp(playerTransform.position, newPos, Smoothfactor);
-        if (lookAtPlayer || rotateAroundPlayer)
-            playerTransform.LookAt(playerTransform);
+        Vector3 newPos = playerTransform.position + cameraOffset;
+        transform.position = Vector3.Slerp(transform.position, newPos, Smoothfactor);
+        if (rotateAroundPlayer)
+            transform.LookAt(playerTransform);
     }
 }
