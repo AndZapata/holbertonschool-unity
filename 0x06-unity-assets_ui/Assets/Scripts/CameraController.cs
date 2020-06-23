eaee50cdb40b359300f5004f6c9cfb6b52ca5321
andzapata <andzapata514@gmail.com>
2020-06-22 22:06:22 -0500

    [Add] fixings

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -5,7 +5,6 @@ public class CameraController : MonoBehaviour
 {
   public Transform playerTransform;
   public float rotationSpeed = 100f;
-  Quaternion camTurnAngle;
   public bool isInverted = false;
   float mouseX;
   float mouseY;
@@ -21,9 +20,9 @@ public class CameraController : MonoBehaviour
   {
     mouseX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
     mouseY += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime * (isInverted ? -1 : 1);
-    Vector3 dir = new Vector3(0, 2f, -5f);
+    Vector3 dir = new Vector3(0, 3f, -6f);
     mouseY = Mathf.Clamp(mouseY, -20f, 20f);
-    camTurnAngle = Quaternion.Euler(mouseY, mouseX, 0);
+    Quaternion camTurnAngle = Quaternion.Euler(mouseY, mouseX, 0);
     transform.position = playerTransform.position + camTurnAngle * dir;
     transform.LookAt(playerTransform);
   }
