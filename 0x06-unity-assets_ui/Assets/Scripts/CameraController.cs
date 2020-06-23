dd67cd394551d19e3597741a5a32ee3fd5b7337a
andzapata <andzapata514@gmail.com>
2020-06-22 21:36:16 -0500

    [Add] Something

--- 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -1,26 +1,30 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-  private Vector3 cameraOffset;
   public Transform playerTransform;
-  public float rotationSpeed = 5f;
+  public float rotationSpeed = 100f;
   Quaternion camTurnAngle;
+  public bool isInverted = false;
+  float mouseX;
+  float mouseY;
+  public Toggle InvertedYMode;
 
   void Start()
   {
-    cameraOffset = transform.position - playerTransform.position;
+    if (InvertedYMode)
+      isInverted = true;
   }
 
   void LateUpdate()
   {
-    if (Input.GetMouseButton(1))
-    {
-      camTurnAngle = Quaternion.Euler(Input.GetAxis("Mouse Y") * rotationSpeed, Input.GetAxis("Mouse X") * rotationSpeed, 0);
-    }
-    cameraOffset = camTurnAngle * cameraOffset;
-    Vector3 newPos = playerTransform.position + cameraOffset;
-    transform.position = newPos;
+    mouseX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+    mouseY += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime * (isInverted ? -1 : 1);
+    Vector3 dir = new Vector3(0, 2f, -5f);
+    mouseY = Mathf.Clamp(mouseY, -20f, 20f);
+    camTurnAngle = Quaternion.Euler(mouseY, mouseX, 0);
+    transform.position = playerTransform.position + camTurnAngle * dir;
     transform.LookAt(playerTransform);
   }
 }
