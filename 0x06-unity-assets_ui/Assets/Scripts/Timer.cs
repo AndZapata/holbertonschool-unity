967593769639db74f508046a51fbfe45a76f1b15
andzapata <andzapata514@gmail.com>
2020-06-25 00:15:14 -0500

    [Fix] Adding some features

--- 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ public class Timer : MonoBehaviour
     public Text TimerText;
     public float milliSeconds, seconds, minutes, intTimer;
 
+    public Text FinalTime;
+
     void Update()
     {
         intTimer += Time.deltaTime;
@@ -14,5 +16,10 @@ public class Timer : MonoBehaviour
         seconds = (int)(intTimer % 60f);
         milliSeconds = (int)(intTimer * 100) % 100;
         TimerText.text = String.Format("{0:0}:{1:00}.{2:00}", minutes, seconds, milliSeconds);
+    }
+
+    public void Win()
+    {
+        FinalTime.text = TimerText.text;
     }
 }
--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -4,14 +4,18 @@ using UnityEngine.UI;
 public class WinTrigger : MonoBehaviour
 {
     public GameObject player;
-    public Text timerText;
+    public GameObject timerUI;
+    public GameObject winMenuUI;
+    //public Text timerText;
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
             player.GetComponent<Timer>().enabled = false;
-            timerText.color = Color.green;
-            timerText.fontSize = 100;
+            timerUI.SetActive(false);
+            winMenuUI.SetActive(true);
+            // timerText.color = Color.green;
+            // timerText.fontSize = 100;
         }
     }
 }
