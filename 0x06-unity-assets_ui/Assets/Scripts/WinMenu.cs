f80d167691f89095df8b8a2e6f85e0a563425e9a
andzapata <andzapata514@gmail.com>
2020-06-25 20:15:29 -0500

    [Fix] Win flag and win menu

---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Next()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 4)
+            MainMenu();
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
--- 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
+++ 0x06-unity-assets_ui/Assets/Scripts/WinTrigger.cs
@@ -6,16 +6,22 @@ public class WinTrigger : MonoBehaviour
     public GameObject player;
     public GameObject timerUI;
     public GameObject winMenuUI;
-    //public Text timerText;
+    public Text FinalTime;
+    public Text timerText;
     void OnTriggerEnter(Collider other)
     {
         if (other.name == "Player")
         {
             player.GetComponent<Timer>().enabled = false;
-            timerUI.SetActive(false);
-            winMenuUI.SetActive(true);
+            Win();
             // timerText.color = Color.green;
             // timerText.fontSize = 100;
         }
+    }
+    public void Win()
+    {
+        timerUI.SetActive(false);
+        winMenuUI.SetActive(true);
+        FinalTime.text = timerText.text;
     }
 }
