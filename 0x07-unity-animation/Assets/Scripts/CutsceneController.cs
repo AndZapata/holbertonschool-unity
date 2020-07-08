6f5860f5c85d480ccb592d2e19f004ac36ed4ef5
andzapata <andzapata514@gmail.com>
2020-07-08 18:35:47 -0500

    [Fix] 6 task

---
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CutsceneController : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
