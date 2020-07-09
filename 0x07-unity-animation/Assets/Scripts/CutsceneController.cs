22f1428aa9ae36e12eccf9a90599c6ff3591ec5f
andzapata <andzapata514@gmail.com>
2020-07-08 19:12:50 -0500

    [Add] Task 6

--- 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
+++ 0x07-unity-animation/Assets/Scripts/CutsceneController.cs
@@ -3,11 +3,18 @@
 public class CutsceneController : MonoBehaviour
 {
     public Animator animator;
+    public GameObject mainCam;
     public GameObject timer;
+    public PlayerController playerScript;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (animator.GetCurrentAnimatorStateInfo(0).length < animator.GetCurrentAnimatorStateInfo(0).normalizedTime)
+        {
+            timer.SetActive(true);
+            playerScript.enabled = true;
+            mainCam.SetActive(true);
+            gameObject.SetActive(false);
+        }
     }
 }
