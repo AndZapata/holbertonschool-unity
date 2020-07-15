530dbe2cebaaee500e725e274daf8b27dbfd13c6
andzapata <andzapata514@gmail.com>
2020-07-15 16:55:16 -0500

    [Add] newest intros + fixed updates for animations on ty

--- 0x07-unity-animation/Assets/Scripts/PlayerController.cs
+++ 0x07-unity-animation/Assets/Scripts/PlayerController.cs
@@ -6,52 +6,69 @@ public class PlayerController : MonoBehaviour
     public float gravity = -9.81f * 2;
     public float jump = 1.8f;
     public float speed = 12f;
-    public float turnSmoothTime = 0.1f;
     Transform mainCamera;
     Transform playerPosition;
-    float turnSmoothVelocity;
     Vector3 vel;
 
     public Animator animator;
-    bool fallingDown = false;
     int jumpHash = Animator.StringToHash("Jump");
+    int fallingHash = Animator.StringToHash("Falling");
+    bool running = false;
 
     void Start()
     {
         playerPosition = GetComponent<Transform>();
         mainCamera = Camera.main.transform;
-        // animator = this.GetComponent<Animator>();
     }
     public void Update()
     {
         if (controller.isGrounded && vel.y < 0)
-            vel.y = 0f;
+            vel.y = -2f;
+
         if (controller.isGrounded && Input.GetButtonDown("Jump"))
         {
+            running = false;
             vel.y = Mathf.Sqrt(jump * -2f * gravity);
-            animator.SetTrigger (jumpHash);
+            animator.SetTrigger(jumpHash);
+            if (playerPosition.position.y < -5f)
+            {
+                animator.SetTrigger(fallingHash);
+            }
         }
-        
+
+        if (running == true)
+            animator.SetBool("Run", true);
+        else
+            animator.SetBool("Run", false);
+
+        Movement();
+
+        if (playerPosition.position.y < -5f)
+        {
+            animator.SetTrigger(fallingHash);
+            if (playerPosition.position.y < -40f)
+            {
+                running = false;
+                playerPosition.position = new Vector3(0, 70, 0);
+            }
+        }
+    }
+    void Movement ()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
         if (direction.magnitude >= 0.1f)
         {
+            running = true;
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + mainCamera.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
-            animator.SetFloat("Vertical", Input.GetAxis("Vertical"));
-            animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         }
+        else
+            running = false;
         vel.y += gravity * Time.deltaTime;
         controller.Move(vel * Time.deltaTime);
-        if (playerPosition.position.y < -30f)
-        {
-            fallingDown = true;
-            playerPosition.position = new Vector3(0, 50, 0);
-            animator.SetBool("FallingDown", fallingDown);
-        }
     }
 }
