1cf59185feccea86475e19ea9ef76b1a3ed282f2
andzapata <andzapata514@gmail.com>
2020-08-08 00:16:42 -0500

    [Add] Layout + First canvas perspective + Script

---
+++ 0x09-unity_ar_business_card/Assets/ButtonsApplication.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ButtonsApplication : MonoBehaviour
+{
+    public void Twitter() {
+        Application.OpenURL("https://twitter.com/AndZapata1");
+    }
+    public void Github() {
+        Application.OpenURL("https://github.com/AndZapata");
+    }
+    public void Email() {
+        Application.OpenURL("mailto:andzapata514@gmail.com");
+    }
+    public void LinkedIn() {
+        Application.OpenURL("https://www.linkedin.com/in/paula-andrea-gutierrez-zapata/");
+    }
+}
